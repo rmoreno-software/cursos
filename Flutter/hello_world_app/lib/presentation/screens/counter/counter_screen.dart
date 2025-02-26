1b76d35fd63013130212779d2627e4c33631ef3b
Roger Moreno <roger.moreno.software.engineer@gmail.com>
2025-02-26 12:40:02 +0100

    S4 - Cambiar el estado de la aplicación

--- Flutter/hello_world_app/lib/presentation/screens/counter/counter_screen.dart
+++ Flutter/hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
 
   @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: const Text('Counter Screen')),
-      ),
+      appBar: AppBar(title: Center(child: const Text('Counter Screen'))),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text('10', style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
-            Text('Clicks', style: TextStyle(fontSize: 25))
-          ]
+          children: [
+            Text(
+              '$clickCounter',
+              style: const TextStyle(
+                fontSize: 160,
+                fontWeight: FontWeight.w100,
+              ),
+            ),
+            const Text('Clicks', style: TextStyle(fontSize: 25)),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-
+          setState(() {
+            clickCounter++;
+          });
         },
         child: const Icon(Icons.plus_one),
       ),
