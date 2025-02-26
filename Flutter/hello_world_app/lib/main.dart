ad0eb77768c21ee5f77af9baf338a8a1ada87267
Roger Moreno <roger.moreno.software.engineer@gmail.com>
2025-02-26 11:12:39 +0100

    S4 - Estructura de directorios - Nueva Pantalla

--- Flutter/hello_world_app/lib/main.dart
+++ Flutter/hello_world_app/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Hola Mundo'))),
+      home: CounterScreen()
     );
   }
 }
