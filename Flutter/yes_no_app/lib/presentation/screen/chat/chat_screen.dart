7b2355fe9f7fd495556bdc7be9eda64d19e2078e
Roger Moreno <roger.moreno.software.engineer@gmail.com>
2025-02-28 08:23:34 +0100

    S5 - ListView - área de los mensajes

--- Flutter/yes_no_app/lib/presentation/screen/chat/chat_screen.dart
+++ Flutter/yes_no_app/lib/presentation/screen/chat/chat_screen.dart
@@ -11,11 +11,36 @@ class ChatScreen extends StatelessWidget {
           padding: const EdgeInsets.all(4.0),
           child: CircleAvatar(
             backgroundImage: NetworkImage(
-              'https://cdn-icons-png.flaticon.com/256/4140/4140051.png'
+              'https://cdn-icons-png.flaticon.com/256/4140/4140051.png',
             ),
           ),
         ),
         title: const Text('Mi amor ❤'),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return Text('Indice: $index');
+                },
+              ),
+            ),
+            Text('Mundo'),
+          ],
+        ),
       ),
     );
   }
