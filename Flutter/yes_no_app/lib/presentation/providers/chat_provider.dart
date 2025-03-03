1fe953e77c816f317bb0b44c57cd197158dcc11b
Roger Moreno <roger.moreno.software.engineer@gmail.com>
2025-03-03 14:00:49 +0100

    S6 - Mostrar los mensajes del proceider

--- Flutter/yes_no_app/lib/presentation/providers/chat_provider.dart
+++ Flutter/yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messages = [
     Message(text: 'Hola amor', fromWho: FromWho.me),
     Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me)
   ];
