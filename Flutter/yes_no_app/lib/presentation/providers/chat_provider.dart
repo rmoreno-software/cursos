8dbe96ba6c3c5950dd6e4900e9ae53ea4aba09d8
Roger Moreno <roger.moreno.software.engineer@gmail.com>
2025-03-03 14:29:49 +0100

    S6 - Mover el Scroll al final

--- Flutter/yes_no_app/lib/presentation/providers/chat_provider.dart
+++ Flutter/yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -2,15 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messages = [
     Message(text: 'Hola amor', fromWho: FromWho.me),
     Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me),
   ];
 
   Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messages.add(newMessage);
 
     notifyListeners();
+    _moveScrollToBottom();
+  }
+
+  Future<void> _moveScrollToBottom() async {
+    await Future.delayed(const Duration(microseconds: 100));
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
   }
 }
--- Flutter/yes_no_app/lib/presentation/widgets/chat/my_message_bubble.dart
+++ Flutter/yes_no_app/lib/presentation/widgets/chat/my_message_bubble.dart
@@ -30,7 +30,7 @@ class MyMessageBubble extends StatelessWidget {
             ),
           ),
         ),
-        const SizedBox(height: 10),
+        const SizedBox(height: 5),
       ],
     );
   }
