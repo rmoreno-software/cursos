0066e3b60d02fa6a8b2389dd7f8189cb06f09718
Roger Moreno <roger.moreno.software.engineer@gmail.com>
2025-03-05 14:28:47 +0100

    S7 - Bonus - Icono girando - animate_do

--- Flutter/toktik/lib/presentation/widgets/video_player/video_buttons.dart
+++ Flutter/toktik/lib/presentation/widgets/video_player/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:toktik/config/helpers/human_formats.dart';
 import 'package:toktik/domain/entities/video_post.dart';
@@ -16,7 +17,14 @@ class VideoButtons extends StatelessWidget {
     return Column(
       children: [
         _CustomIconButton(value: video.likes, iconData: Icons.favorite, iconColor: Colors.red),
-        _CustomIconButton(value: video.views, iconData: Icons.remove_red_eye_outlined)
+        const SizedBox(height: 20),
+        _CustomIconButton(value: video.views, iconData: Icons.remove_red_eye_outlined),
+        const SizedBox(height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration( seconds: 5),
+          child: _CustomIconButton(value: 0, iconData: Icons.play_circle_outline)
+        )
       ],
     );
   }
@@ -41,7 +49,8 @@ class _CustomIconButton extends StatelessWidget {
         IconButton(
           onPressed: () {},
           icon: Icon(iconData, color: color, size: 30)),
-        Text(HumanFormats.humanReadbleNumber(value.toDouble()))
+        if( value > 0)
+          Text(HumanFormats.humanReadbleNumber(value.toDouble()))
       ]
     );
   }
