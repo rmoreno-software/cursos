a1490c507527f33fa323ac7d3257fe19e0c91ade
Roger Moreno <roger.moreno.software.engineer@gmail.com>
2025-03-06 13:48:56 +0100

    S7 - Gradiente de fondo

--- Flutter/toktik/lib/presentation/widgets/video_player/full_screen_player.dart
+++ Flutter/toktik/lib/presentation/widgets/video_player/full_screen_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toktik/presentation/widgets/video_player/video_background.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenPlayer extends StatefulWidget {
@@ -59,6 +60,9 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
             child: Stack(
               children: [
                 VideoPlayer(controller),
+                VideoBackground(
+                  stops: [0.8, 1.0],
+                ),
                 Positioned(
                   bottom: 50,
                   left: 20,
