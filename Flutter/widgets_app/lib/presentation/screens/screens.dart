3e143df04f0a2a2c5c763e526f8474b8d363a8e0
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-03-10 14:59:12 +0100

    S9 - Crear todas las pantallas restantes

--- Flutter/widgets_app/lib/presentation/screens/screens.dart
+++ Flutter/widgets_app/lib/presentation/screens/screens.dart
@@ -1,3 +1,10 @@
+
+export 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_app/presentation/screens/app_tutotial/app_tutorial_screen.dart';
 export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/screens/cards/cards_screens.dart';
 export 'package:widgets_app/presentation/screens/home/home_screen.dart';
+export 'package:widgets_app/presentation/screens/ininite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_app/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/presentation/screens/ui_controls/ui_controls_screen.dart';
