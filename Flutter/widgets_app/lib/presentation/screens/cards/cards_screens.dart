94cc85f4ce1e8ab5d02e0743124de0990132826c
Roger Moreno <roger.moreno.software.engineer@gmail.com>
2025-03-10 11:41:32 +0100

    S9 - Navegación entre pantallas

---
+++ Flutter/widgets_app/lib/presentation/screens/cards/cards_screens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CardsSceen extends StatelessWidget {
+  const CardsSceen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cards Screen'),
+      ),
+      body: Placeholder(),
+    );
+  }
+}
