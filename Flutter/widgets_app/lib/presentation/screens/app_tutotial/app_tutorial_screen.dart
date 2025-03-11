1419961232e31be7f38963d9b90169a50fe170ae
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-03-11 12:33:52 +0100

    S10 - Finalizar tutorial

--- Flutter/widgets_app/lib/presentation/screens/app_tutotial/app_tutorial_screen.dart
+++ Flutter/widgets_app/lib/presentation/screens/app_tutotial/app_tutorial_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 
 class SlideInfo {
@@ -25,15 +26,29 @@ class AppTutorialScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        physics: BouncingScrollPhysics(),
-        children: slides.map(
-          (slideData) => _Slide(
-            title: slideData.title,
-            caption: slideData.caption,
-            imageUrl: slideData.imageUrl,
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          PageView(
+            physics: BouncingScrollPhysics(),
+            children: slides.map(
+              (slideData) => _Slide(
+                title: slideData.title,
+                caption: slideData.caption,
+                imageUrl: slideData.imageUrl,
+              )
+            ).toList(),
+          ),
+
+          Positioned(
+            right: 20,
+            top: 50,
+            child: TextButton(
+              onPressed: context.pop,
+              child: const Text('Salir')
+            )
           )
-        ).toList(),
+        ],
       )
     );
   }
@@ -53,6 +68,25 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(image: AssetImage(imageUrl)),
+            const SizedBox(height: 20),
+            Text(title, style: titleStyle),
+            const SizedBox(height: 20),
+            Text(caption, style: captionStyle)
+          ],
+        )
+      ),
+    );
   }
 }
