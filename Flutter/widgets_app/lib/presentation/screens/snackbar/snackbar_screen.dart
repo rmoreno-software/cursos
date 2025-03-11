ab0e295597924bf87deef6ca7dc9b34c09ce9977
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-03-11 11:14:58 +0100

    S10 - Diálogos y Licencias

--- Flutter/widgets_app/lib/presentation/screens/snackbar/snackbar_screen.dart
+++ Flutter/widgets_app/lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackBarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
@@ -22,6 +23,21 @@ class SnackBarScreen extends StatelessWidget {
     ).showSnackBar(snackbar);
   }
 
+  void _openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('¿Estás seguro?'),
+        content: const Text('Incididunt dolor cupidatat est culpa aute incididunt Lorem dolore tempor esse non cillum duis. Nisi culpa velit non nostrud cupidatat aliqua ipsum pariatur enim non irure exercitation fugiat. Tempor consectetur aliqua sit fugiat laboris est sint. Excepteur exercitation aute est esse.'),
+      actions: [
+        TextButton(onPressed: () => context.pop(), child: const Text('Cancelar')),
+        FilledButton(onPressed: () => context.pop(), child: const Text('Aceptar'))
+      ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +47,23 @@ class SnackBarScreen extends StatelessWidget {
         icon: Icon(Icons.remove_red_eye_outlined),
         label: const Text('Mostrar Snackbar'),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            /*FilledButton.tonal(
+              onPressed: (){
+                showAboutDialog(context: context);
+              },
+              child: const Text('Licencias usadas')
+            ),*/
+            FilledButton.tonal(
+              onPressed: () => _openDialog(context),
+              child: const Text('Mostrar diálogo')
+            )
+          ]
+        ),
+      )
     );
   }
 }
