4192e9adc747adc9f6fbf9d908d3d75e88113d41
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-03-11 13:09:54 +0100

    S10 - Infinite Scroll

--- Flutter/widgets_app/lib/config/menu/menu_items.dart
+++ Flutter/widgets_app/lib/config/menu/menu_items.dart
@@ -59,5 +59,11 @@ const appMenuItems = <MenuItem>[
     subTitle: 'Pequeño tutorial introductorio',
     link: '/tutorial',
     icon: Icons.accessible_rounded
+  ),
+  MenuItem(
+    title: 'InfiniteScroll y Pull',
+    subTitle: 'Listas infinitas y pull to refresh',
+    link: '/infinite',
+    icon: Icons.list_alt_rounded
   )
 ];
--- Flutter/widgets_app/lib/presentation/screens/ininite_scroll/infinite_scroll_screen.dart
+++ Flutter/widgets_app/lib/presentation/screens/ininite_scroll/infinite_scroll_screen.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class InfiniteScrollScreen extends StatelessWidget {
+class InfiniteScrollScreen extends StatefulWidget {
 
   static const name = 'ifinite_scroll_screen';
   
   const InfiniteScrollScreen({super.key});
 
   @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+
+  List<int> imagesIds = [1, 2, 3, 4, 5];
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: ListView.builder(
+          itemCount: imagesIds.length,
+          itemBuilder: (context, index) {
+            return FadeInImage(
+              width: double.infinity,
+              fit: BoxFit.cover,
+              height: 300,
+              placeholder: AssetImage('assets/images/jar-loading.gif'),
+              image: NetworkImage('https://picsum.photos/id/${imagesIds[index]}/500/300')
+            );
+          },
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.pop(),
+        child: Icon(Icons.arrow_back_ios_new_outlined)
+      ),
+    );
   }
 }
