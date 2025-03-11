18b568d8b082a816001d43ea69aeb52a92a09070
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-03-11 11:38:49 +0100

    S10 - Animar propiedades del contenedor

--- Flutter/widgets_app/lib/presentation/screens/animated/animated_screen.dart
+++ Flutter/widgets_app/lib/presentation/screens/animated/animated_screen.dart
@@ -1,10 +1,41 @@
+import 'dart:math' show Random;
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
 
   static const name = 'animated_screen';
   
   const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+
+  double width = 50;
+  double height = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+
+  void changeShape() {
+      final random = Random();
+
+      width = random.nextInt(300) + 120;
+      height = random.nextInt(300) + 120;
+      borderRadius = random.nextInt(100) + 20;
+
+      color = Color.fromRGBO(
+        random.nextInt(255),
+        random.nextInt(255),
+        random.nextInt(255),
+        1
+      );
+
+      setState(() {
+        
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,19 +46,19 @@ class AnimatedScreen extends StatelessWidget {
       ),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: changeShape,
         child: const Icon(Icons.play_arrow_rounded),
       ),
 
       body: Center(
         child: AnimatedContainer(
-          duration: const Duration(microseconds: 400),
-          curve: Curves.easeOutCubic,
-          width: 100,
-          height: 100,
+          duration: const Duration(milliseconds: 400),
+          curve: Curves.elasticInOut,
+          width: width <= 0 ? 0 : width,
+          height: height <= 0 ? 0 : height,
           decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(20)
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius < 0 ? 0 : borderRadius)
           ),
         ),
       )
