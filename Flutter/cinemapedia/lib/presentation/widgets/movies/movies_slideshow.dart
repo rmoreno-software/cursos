8ddd801bae9cae1125ac623bfeebed7f2f8ce1ed
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-03-26 12:43:57 +0100

    S13 - Swiper Pagination

--- Flutter/cinemapedia/lib/presentation/widgets/movies/movies_slideshow.dart
+++ Flutter/cinemapedia/lib/presentation/widgets/movies/movies_slideshow.dart
@@ -14,6 +14,9 @@ class MoviesSlideshow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
     return SizedBox(
       height: 210,
       width: double.infinity,
@@ -21,6 +24,13 @@ class MoviesSlideshow extends StatelessWidget {
         viewportFraction: 0.8,
         scale: 0.9,
         autoplay: true,
+        pagination: SwiperPagination(
+          margin: EdgeInsets.only(top: 0),
+          builder: DotSwiperPaginationBuilder(
+            activeColor: colors.primary,
+            color: colors.secondary
+          )
+        ),
         itemCount: movies.length,
         itemBuilder: (context, index) {
           final movie = movies[index];
