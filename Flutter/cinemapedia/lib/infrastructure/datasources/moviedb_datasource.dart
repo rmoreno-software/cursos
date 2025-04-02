94586b07cda4b3312baf78c4c60ef5b8648a6e46
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-04-02 13:07:10 +0200

    S14 - Obtener película por ID - Datasource. MovieDB to Entity

--- Flutter/cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
+++ Flutter/cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
@@ -2,6 +2,7 @@ import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details_moviedb.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
@@ -51,6 +52,19 @@ class MoviedbDatasource extends MoviesDatasource{
     });
 
     return _jsonToMovies(response.data);
+  }
+
+   @override
+  Future<Movie> getMovieById(String id) async{
+    
+    final response = await dio.get('/movie/${id}');
+    if ( response.statusCode != 200 ) throw Exception('Movie with id: $id not found');
+
+    final movieDB = MovieDetailsMoviedb.fromJson(response.data);
+
+    final movie = MovieMapper.movieDetailsToEntity(movieDB);
+
+    return movie;
   }
 
   List<Movie> _jsonToMovies( Map<String, dynamic> json) {
