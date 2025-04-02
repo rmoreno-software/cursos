4416112f1da6821a2a7936dd9f1abe808b797752
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-04-02 13:26:27 +0200

    S14 - Movie Details - Caché Local

---
+++ Flutter/cinemapedia/lib/presentation/providers/movies/movie_info_provider.dart
@@ -0,0 +1,37 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final movieInfoProvider = StateNotifierProvider<MovieMapNotifier, Map<String, Movie>>((ref) {
+  
+  final getMovie = ref.watch(moviesRepositoryProvider).getMovieById;
+
+  return MovieMapNotifier(getMovie: getMovie);
+});
+
+/*
+  '505642': Movie(),
+  '505643': Movie(),
+  '505644': Movie(),
+  '505645': Movie(),
+*/
+
+typedef GetMovieCallback = Future<Movie>Function(String movieId);
+
+class MovieMapNotifier extends StateNotifier<Map<String, Movie>> {
+
+  final GetMovieCallback getMovie;
+
+  MovieMapNotifier({
+    required this.getMovie
+  }): super({});
+
+  Future<void> loadMovie(String movieId) async {
+    if ( state[movieId] != null) return;
+
+    final movie = await getMovie(movieId);
+
+    state = {...state, movieId: movie};
+  }
+
+}
--- Flutter/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
+++ Flutter/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends StatefulWidget {
 
   static const name = 'movie-screen';
 
@@ -12,10 +12,21 @@ class MovieScreen extends StatelessWidget {
   });
 
   @override
+  State<MovieScreen> createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('MovieID: $movieId'),
+        title: Text('MovieID: ${widget.movieId}'),
       ),
       body: Placeholder(),
     );
