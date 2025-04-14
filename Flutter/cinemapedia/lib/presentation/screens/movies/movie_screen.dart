a5e8372a522424da03f8e3745dadfd56de2fa7d7
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-04-14 10:41:46 +0200

    S14 - Mostrar los actores de la película

--- Flutter/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
+++ Flutter/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
@@ -109,8 +109,9 @@ class _MovieDetails extends StatelessWidget {
             ],
           ),
         ),
-        // TODO: Mostrar actores ListView
-        SizedBox(height: 100),
+        _ActorsByMovie(movieId: movie.id.toString()),
+
+        const SizedBox(height: 50)
       ],
     );
   }
@@ -130,11 +131,11 @@ class _CustomSliverAppBar extends StatelessWidget {
       foregroundColor: Colors.white,
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        title: Text(
+        /*title: Text(
           movie.title,
           style: const TextStyle(color: Colors.white, fontSize: 20),
           textAlign: TextAlign.start,
-        ),
+        ),*/
         background: Stack(
           children: [
             SizedBox.expand(
@@ -170,4 +171,61 @@ class _CustomSliverAppBar extends StatelessWidget {
       ),
     );
   }
+}
+
+class _ActorsByMovie extends ConsumerWidget {
+  
+  final String movieId;
+
+  const _ActorsByMovie({required this.movieId});
+  
+  @override
+  Widget build(BuildContext context, ref) {
+
+    final actorsByMovie = ref.watch(actorsByMovieProvider);
+
+    if ( actorsByMovie[movieId] == null) {
+      return const CircularProgressIndicator(strokeWidth: 2);
+    }
+
+    final actors = actorsByMovie[movieId]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors.length,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+
+          return Container(
+            padding: EdgeInsets.all(8.0),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      actor.profilePath,
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(actor.name, maxLines: 2),
+                  Text(
+                    actor.character ?? '',
+                    maxLines: 2,
+                    style: TextStyle(fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis),
+                  )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
 }
