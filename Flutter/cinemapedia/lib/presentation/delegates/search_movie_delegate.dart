b77b8d6d545e2636a11bc4e9e4233d2a0795f14d
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-04-30 10:26:14 +0200

    S15 - SearchDelegate

---
+++ Flutter/cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SearchMovieDelegate extends SearchDelegate {
+
+  @override
+  String get searchFieldLabel => 'Buscar película';
+  
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      const Text('buildActions')
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return const Text('builLeading');
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text('buildResults');
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return const Text('buildSuggestions');
+  }
+
+}
