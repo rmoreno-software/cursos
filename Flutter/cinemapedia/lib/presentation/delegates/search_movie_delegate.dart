773454db82b23d35119c22239a6a715a3ebb2796
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-04-30 13:57:47 +0200

    S15 - Regresar de la búsqueda con argumentos

--- Flutter/cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
+++ Flutter/cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
@@ -54,7 +54,10 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
           itemCount: movies.length,
           itemBuilder: (context, index) {
             final movie = movies[index];
-            return _MovieItem(movie: movie);
+            return _MovieItem(
+              movie: movie,
+              onMovieSelected: close,
+            );
           },
         );
       },
@@ -66,9 +69,11 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 class _MovieItem extends StatelessWidget {
 
   final Movie movie;
+  final Function onMovieSelected;
 
   const _MovieItem({
-    required this.movie
+    required this.movie,
+    required this.onMovieSelected
   });
 
   @override
@@ -77,48 +82,53 @@ class _MovieItem extends StatelessWidget {
     final textStyles = Theme.of(context).textTheme;
     final size = MediaQuery.of(context).size;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Row(
-        children: [
-          // Image
-          SizedBox(
-            width: size.width * 0.2,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.network(
-                movie.posterPath,
-                loadingBuilder: (context, child, loadingProgress) => FadeIn(child: child),
+    return GestureDetector(
+      onTap: () {
+        onMovieSelected(context, movie);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          children: [
+            // Image
+            SizedBox(
+              width: size.width * 0.2,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  movie.posterPath,
+                  loadingBuilder: (context, child, loadingProgress) => FadeIn(child: child),
+                ),
               ),
             ),
-          ),
-
-          SizedBox(width: 10),
-
-          // Description
-          SizedBox(
-            width: size.width * 0.7,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(movie.title, style:  textStyles.titleMedium),
-                (movie.overview.length > 100)
-                  ? Text('${movie.overview.substring(0,100)}...')
-                  : Text(movie.overview),
-                Row(
-                  children: [
-                    Icon( Icons.star_half_rounded, color: Colors.yellow.shade900,),
-                    SizedBox(width: 5),
-                    Text(
-                      HumanFormats.number(movie.voteAverage, 1),
-                      style: textStyles.bodyMedium!.copyWith(color: Colors.yellow.shade900),
-                    )
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
+      
+            SizedBox(width: 10),
+      
+            // Description
+            SizedBox(
+              width: size.width * 0.7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(movie.title, style:  textStyles.titleMedium),
+                  (movie.overview.length > 100)
+                    ? Text('${movie.overview.substring(0,100)}...')
+                    : Text(movie.overview),
+                  Row(
+                    children: [
+                      Icon( Icons.star_half_rounded, color: Colors.yellow.shade900,),
+                      SizedBox(width: 5),
+                      Text(
+                        HumanFormats.number(movie.voteAverage, 1),
+                        style: textStyles.bodyMedium!.copyWith(color: Colors.yellow.shade900),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- Flutter/cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
+++ Flutter/cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,7 +1,9 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
@@ -26,16 +28,19 @@ class CustomAppbar extends ConsumerWidget {
               const Spacer(),
               IconButton(
                 icon: const Icon(Icons.search),
-                onPressed: () {
+                onPressed: (){
 
                   final movieRepository = ref.read(moviesRepositoryProvider);
 
-                  showSearch(
+                  showSearch<Movie?>(
                     context: context,
                     delegate: SearchMovieDelegate(
                       searchMovies: movieRepository.searchMovies
                     )
-                  );
+                  ).then((movie) {
+                    if (movie == null) return;
+                    context.push('/movie/${movie.id}');
+                  });
                 }
               )
             ],
