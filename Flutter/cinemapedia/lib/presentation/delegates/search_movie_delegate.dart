6900b7df79a90afbc2f0cbef8f31d632717780d8
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-04-30 13:08:14 +0200

    S15 - Mostrar calificación de la película

--- Flutter/cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
+++ Flutter/cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -84,7 +85,7 @@ class _MovieItem extends StatelessWidget {
           SizedBox(
             width: size.width * 0.2,
             child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
+              borderRadius: BorderRadius.circular(10),
               child: Image.network(
                 movie.posterPath,
                 loadingBuilder: (context, child, loadingProgress) => FadeIn(child: child),
@@ -103,7 +104,17 @@ class _MovieItem extends StatelessWidget {
                 Text(movie.title, style:  textStyles.titleMedium),
                 (movie.overview.length > 100)
                   ? Text('${movie.overview.substring(0,100)}...')
-                  : Text(movie.overview)
+                  : Text(movie.overview),
+                Row(
+                  children: [
+                    Icon( Icons.star_half_rounded, color: Colors.yellow.shade900,),
+                    SizedBox(width: 5),
+                    Text(
+                      HumanFormats.number(movie.voteAverage, 1),
+                      style: textStyles.bodyMedium!.copyWith(color: Colors.yellow.shade900),
+                    )
+                  ],
+                )
               ],
             ),
           )
