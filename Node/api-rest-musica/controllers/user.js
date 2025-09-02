bbaa62e872501f802a28ad08f1dfa9b9265ba796
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-09-02 10:15:07 +0200

    Curs Node - Section 8 - Classe 123

--- Node/api-rest-musica/controllers/user.js
+++ Node/api-rest-musica/controllers/user.js
@@ -2,6 +2,8 @@ const validate = require("../helpers/validate");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("../helpers/jwt");
+const fs = require("fs");
+const path = require("path");
 
 const test = (req, res) => {
     return res.status(200).send({
@@ -70,8 +72,8 @@ const login = async (req, res) => {
 
     try {
         const user = await User.findOne({ email: params.email })
-                            .select("+password +role")
-                            .exec();
+            .select("+password +role")
+            .exec();
 
         if (!user) {
             return res.status(404).send({
@@ -81,7 +83,7 @@ const login = async (req, res) => {
         }
 
         const pwd = bcrypt.compareSync(params.password, user.password);
-        if(!pwd) {
+        if (!pwd) {
             return res.status(400).send({
                 status: "error",
                 message: "Incorrect password"
@@ -113,7 +115,7 @@ const profile = async (req, res) => {
 
     try {
         const user = await User.findById(id);
-        if(!user){
+        if (!user) {
             return res.status(404).send({
                 status: "error",
                 message: "User not found"
@@ -133,9 +135,130 @@ const profile = async (req, res) => {
     }
 }
 
+const update = async (req, res) => {
+    let userIdentity = req.user;
+    let userBody = req.body;
+
+    try {
+        if (userBody.email) {
+            let users = await User.find({
+                $or: [
+                    { email: userBody.email.toLowerCase() },
+                ]
+            });
+
+            if (users.length > 0) {
+                return res.status(500).send({
+                    status: "error",
+                    message: "Email already in use"
+                });
+            }
+        }
+
+        if (userBody.nick) {
+            let users = await User.find({
+                $or: [
+                    { nick: userBody.nick.toLowerCase() },
+                ]
+            });
+
+            if (users.length > 0) {
+                return res.status(500).send({
+                    status: "error",
+                    message: "Nick already in use"
+                });
+            }
+        }
+
+        if (userBody.password) {
+            let pwd = await bcrypt.hash(userBody.password, 10);
+            userBody.password = pwd;
+        } else {
+            delete userBody.password;
+        }
+
+        let userUpdated = await User.findByIdAndUpdate({ _id: userIdentity.id }, userBody, { new: true });
+        return res.status(200).send({
+            status: "success",
+            user: userUpdated
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            status: "error",
+            message: "Error updating user"
+        });
+    }
+}
+
+const upload = async (req, res) => {
+
+    try {
+        if (!req.file) {
+            return res.status(500).send({
+                status: "error",
+                message: "Request must include a file"
+            });
+        }
+
+        let image = req.file.originalname;
+
+        const imageSplit = image.split("\.");
+        const extension = imageSplit[1];
+
+        if (extension != "png" && extension != "jpg" && extension != "jpeg" && extension != "gif") {
+            const filePath = req.file.path;
+            const fileDeleted = fs.unlinkSync(filePath);
+            return res.status(500).send({
+                status: "error",
+                message: "Extension is not valid"
+            });
+        }
+
+        let updatedUser = await User.findByIdAndUpdate({ _id: req.user.id }, { image: req.file.filename }, { new: true });
+
+        return res.status(200).send({
+            status: "success",
+            user: updatedUser
+        });
+    } catch (error) {
+        return res.status(500).send({
+            status: "error",
+            message: "Error uploading avatar"
+        });
+    }
+}
+
+const avatar = async (req, res) => {
+    try {
+        const file = req.params.file;
+        const filePath = `./uploads/avatars/${file}`;
+
+        fs.stat(filePath, (error, exists) => {
+            if (error || !exists) {
+                return res.status(404).send({
+                    status: "error",
+                    message: "File not found"
+                });
+            }
+
+            return res.sendFile(path.resolve(filePath));
+        });
+
+    } catch (error) {
+        return res.status(500).send({
+            status: "error",
+            message: "Error getting avatar"
+        });
+    }
+}
+
 module.exports = {
     test,
     register,
     login,
-    profile
+    profile,
+    update,
+    upload,
+    avatar
 }
--- Node/api-rest-musica/routes/user.js
+++ Node/api-rest-musica/routes/user.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const userController = require("../controllers/user");
 const check = require("../middlewares/auth");
+const multer = require("multer");
 
 const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads/avatars/")
+    },
+    filename: (req, file, cb) => {
+        cb(null, `avatar-${Date.now()}-${file.originalname}`);
+    }
+});
+
+const uploads = multer({storage});
 
 router.get("/test", userController.test);
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.get("/profile/:id", check.auth, userController.profile);
+router.put("/update", check.auth, userController.update);
+router.post("/upload", [check.auth, uploads.single("file0")], userController.upload);
+router.get("/avatar/:file", check.auth, userController.avatar);
 
 module.exports = router;
