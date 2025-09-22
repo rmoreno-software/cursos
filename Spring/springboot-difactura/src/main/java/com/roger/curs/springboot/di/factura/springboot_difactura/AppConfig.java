a6848d52d92fea72bf358063d4f9b3f781fe4577
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-09-22 12:14:26 +0200

    Curs Spring - Secció 4 - Classe 72 - Registrando otras líneas para la factura

--- Spring/springboot-difactura/src/main/java/com/roger/curs/springboot/di/factura/springboot_difactura/AppConfig.java
+++ Spring/springboot-difactura/src/main/java/com/roger/curs/springboot/di/factura/springboot_difactura/AppConfig.java
@@ -22,4 +22,17 @@ public class AppConfig {
                 new Item(p2, 4));
     }
 
+    @Bean
+    List<Item> itemsInvoiceOffice() {
+        Product p1 = new Product("Monitor asus 24", 700);
+        Product p2 = new Product("Notebook racer", 2400);
+        Product p3 = new Product("Impresora HP", 800);
+        Product p4 = new Product("Escritorio", 900);
+        return Arrays.asList(
+                new Item(p1, 4),
+                new Item(p2, 6),
+                new Item(p3, 1),
+                new Item(p4, 4));
+    }
+
 }
--- Spring/springboot-difactura/src/main/java/com/roger/curs/springboot/di/factura/springboot_difactura/models/Invoice.java
+++ Spring/springboot-difactura/src/main/java/com/roger/curs/springboot/di/factura/springboot_difactura/models/Invoice.java
@@ -1,6 +1,7 @@
 package com.roger.curs.springboot.di.factura.springboot_difactura.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,7 @@ public class Invoice {
     @Value("${invoice.description}")
     private String description;
     @Autowired
+    @Qualifier("itemsInvoiceOffice")
     private List<Item> items;
 
     public Client getClient() {
