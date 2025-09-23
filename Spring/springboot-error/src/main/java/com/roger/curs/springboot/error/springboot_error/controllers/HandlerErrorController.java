8e89c4f2c7a591f8524b025a7fc6217177675657
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-09-23 10:23:13 +0200

    Curs Spring - Secció 5 - Classe 81 - Añadiendo método handler para el manejo de excepción

--- Spring/springboot-error/src/main/java/com/roger/curs/springboot/error/springboot_error/controllers/HandlerErrorController.java
+++ Spring/springboot-error/src/main/java/com/roger/curs/springboot/error/springboot_error/controllers/HandlerErrorController.java
@@ -1,14 +1,24 @@
 package com.roger.curs.springboot.error.springboot_error.controllers;
 
+import com.roger.curs.springboot.error.springboot_error.models.Error;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Date;
 
 @RestControllerAdvice
 public class HandlerErrorController {
 
     @ExceptionHandler(ArithmeticException.class)
-    public ResponseEntity<?> divisionByZero(Exception ex) {
-        return ResponseEntity.internalServerError().body("Error 500");
+    public ResponseEntity<com.roger.curs.springboot.error.springboot_error.models.Error> divisionByZero(Exception ex) {
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Error divisió per cero!");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        // return ResponseEntity.internalServerError().body(error);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
     }
 }
