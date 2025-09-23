bce8923601cba1cd50a2ccebf48f8359b10a419e
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-09-23 14:27:16 +0200

    Curs Spring - Secció 6(Interceptores HTTP) - Classe 94(Implementando interceptor TiempoTranscurrido)

--- Spring/sprinboot-interceptor/src/main/java/com/roger/curso/springboot/interceptor/sprinboot_interceptor/interceptors/LoadingTimeInterceptor.java
+++ Spring/sprinboot-interceptor/src/main/java/com/roger/curso/springboot/interceptor/sprinboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -9,6 +9,8 @@ import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Random;
+
 @Component("timeInterceptor")
 public class LoadingTimeInterceptor implements HandlerInterceptor {
 
@@ -18,11 +20,24 @@ public class LoadingTimeInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HandlerMethod controller = ((HandlerMethod) handler);
         logger.info("LoadingTimeInterceptor: preHandle() entrando... " + controller.getMethod().getName());
+
+        long start = System.currentTimeMillis();
+        request.setAttribute("start", start);
+
+        Random random = new Random();
+        int delay = random.nextInt(500);
+        Thread.sleep(delay);
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        long end = System.currentTimeMillis();
+        long start = (long) request.getAttribute("start");
+        long result = end - start;
+
+        logger.info("Tiempo trascurrido: " + result + " milisegundos");
+
         HandlerMethod controller = ((HandlerMethod) handler);
         logger.info("LoadingTimeInterceptor: postHandle() saliendo... " + controller.getMethod().getName());
     }
