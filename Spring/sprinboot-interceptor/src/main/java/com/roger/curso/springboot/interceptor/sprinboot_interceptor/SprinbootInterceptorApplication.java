36df8ccdae650b83e69bfc7075ce58e9d63d1ce6
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-09-23 14:05:41 +0200

    Curs Spring - Secció 6(Interceptores HTTP) - Classe 91(Creando Proyecto)

---
+++ Spring/sprinboot-interceptor/src/main/java/com/roger/curso/springboot/interceptor/sprinboot_interceptor/SprinbootInterceptorApplication.java
@@ -0,0 +1,13 @@
+package com.roger.curso.springboot.interceptor.sprinboot_interceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootInterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootInterceptorApplication.class, args);
+	}
+
+}
