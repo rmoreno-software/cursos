0f5a05f40e7b041694aa1aec7426a2c7d7530be8
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-09-23 10:11:30 +0200

    Curs Spring - Secció 5 - Classe 79 - Creando clase DTO de Error

---
+++ Spring/springboot-error/src/main/java/com/roger/curs/springboot/error/springboot_error/controllers/AppController.java
@@ -0,0 +1,15 @@
+package com.roger.curs.springboot.error.springboot_error.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping("/app")
+    public String index() {
+        int value = 100 / 0;
+        System.out.println(value);
+        return "ok 200";
+    }
+}
