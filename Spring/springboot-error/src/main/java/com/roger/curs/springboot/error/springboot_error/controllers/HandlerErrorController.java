c005890e76b92bfddf46674c667414f89648db77
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-09-23 10:30:43 +0200

    Curs Spring - Secció 5 - Classe 82 - Personalizando error 404

--- Spring/springboot-error/src/main/java/com/roger/curs/springboot/error/springboot_error/controllers/HandlerErrorController.java
+++ Spring/springboot-error/src/main/java/com/roger/curs/springboot/error/springboot_error/controllers/HandlerErrorController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.Date;
 
@@ -12,7 +13,7 @@ import java.util.Date;
 public class HandlerErrorController {
 
     @ExceptionHandler(ArithmeticException.class)
-    public ResponseEntity<com.roger.curs.springboot.error.springboot_error.models.Error> divisionByZero(Exception ex) {
+    public ResponseEntity<Error> divisionByZero(Exception ex) {
         Error error = new Error();
         error.setDate(new Date());
         error.setError("Error divisió per cero!");
@@ -20,5 +21,16 @@ public class HandlerErrorController {
         error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         // return ResponseEntity.internalServerError().body(error);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFoundException(NoHandlerFoundException ex) {
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Api rest no encontrada!");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
     }
 }
