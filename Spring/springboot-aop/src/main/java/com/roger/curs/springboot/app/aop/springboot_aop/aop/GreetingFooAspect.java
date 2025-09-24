3dd8a612da1ec99c8e6652a9dd4aee043e40cba9
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-09-24 14:25:22 +0200

    Curs Spring - Secció 8 (Programación Orientada a Aspectos (AOP)) - Classe 110 (Ordenación de Aspectos con @Order)

---
+++ Spring/springboot-aop/src/main/java/com/roger/curs/springboot/app/aop/springboot_aop/aop/GreetingFooAspect.java
@@ -0,0 +1,30 @@
+package com.roger.curs.springboot.app.aop.springboot_aop.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Order(2)
+@Component
+@Aspect
+public class GreetingFooAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Before("execution(* com.roger.curs.springboot.app.aop.springboot_aop.services.*.*(..))")
+    public void loggerBefore(JoinPoint joinPoint) {
+
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        logger.info("FOO Antes: " + method + " con los argumentos " + args);
+
+    }
+
+}
