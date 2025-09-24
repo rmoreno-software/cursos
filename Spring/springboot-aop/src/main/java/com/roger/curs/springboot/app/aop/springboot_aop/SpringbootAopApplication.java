02bdcf09ae1a715cf5eb71a414e4e51a3cba6933
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-09-24 13:29:28 +0200

    Curs Spring - Secció 8 (Programación Orientada a Aspectos (AOP)) - Classe 106 (Creando el componente Aspect)

--- Spring/springboot-aop/src/main/java/com/roger/curs/springboot/app/aop/springboot_aop/SpringbootAopApplication.java
+++ Spring/springboot-aop/src/main/java/com/roger/curs/springboot/app/aop/springboot_aop/SpringbootAopApplication.java
@@ -2,6 +2,7 @@ package com.roger.curs.springboot.app.aop.springboot_aop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 public class SpringbootAopApplication {
---
+++ Spring/springboot-aop/src/main/java/com/roger/curs/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
@@ -0,0 +1,27 @@
+package com.roger.curs.springboot.app.aop.springboot_aop.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class GreetingAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Before("execution(String com.roger.curs.springboot.app.aop.springboot_aop.services.GreetingService.sayHello(..))")
+    public void loggerBefore(JoinPoint joinPoint) {
+
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        logger.info("Antes: " + method + " con los argumentos " + args);
+
+    }
+}
