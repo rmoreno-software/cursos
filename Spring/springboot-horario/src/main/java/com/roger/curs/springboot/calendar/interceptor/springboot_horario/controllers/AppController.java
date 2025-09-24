23e37f93fe0c17b1b142a416c7fa4843ff90a51a
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-09-24 08:56:55 +0200

    Curs Spring - Secció 7 (Interceptores HTTP Horario de acceso a clientes) - Classe 99 (Creando interceptor horario)

--- Spring/springboot-horario/src/main/java/com/roger/curs/springboot/calendar/interceptor/springboot_horario/controllers/AppController.java
+++ Spring/springboot-horario/src/main/java/com/roger/curs/springboot/calendar/interceptor/springboot_horario/controllers/AppController.java
@@ -1,5 +1,6 @@
 package com.roger.curs.springboot.calendar.interceptor.springboot_horario.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +15,11 @@ import java.util.Map;
 public class AppController {
 
     @GetMapping("/foo")
-    public ResponseEntity<?> foo() {
+    public ResponseEntity<?> foo(HttpServletRequest request) {
         Map<String, Object> data = new HashMap<>();
         data.put("title", "Bienvenidos al sistema de atención");
         data.put("time", new Date());
+        data.put("message", request.getAttribute("message"));
         return ResponseEntity.ok(data);
     }
 }
---
+++ Spring/springboot-horario/src/main/java/com/roger/curs/springboot/calendar/interceptor/springboot_horario/interceptors/CalendarInterceptor.java
@@ -0,0 +1,45 @@
+package com.roger.curs.springboot.calendar.interceptor.springboot_horario.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Calendar;
+
+@Component
+public class CalendarInterceptor implements HandlerInterceptor {
+
+    @Value("${config.calendar.open}")
+    private Integer open;
+    @Value("${config.calendar.close}")
+    private Integer close;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+
+        if (hour >= open && hour < close) {
+            StringBuilder message = new StringBuilder("Bienvenidos al horario de atención a clientes");
+            message.append(", atendemos des de las ");
+            message.append(open);
+            message.append("hrs. ");
+            message.append("hasta las ");
+            message.append(close);
+            message.append("hrs. ");
+            message.append("Gracias por su visita");
+            request.setAttribute("message", message.toString());
+            return true;
+        }
+
+
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+}
