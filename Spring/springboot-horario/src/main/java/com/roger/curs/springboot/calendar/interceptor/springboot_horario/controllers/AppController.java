139b601663e9c695eab426f76d2f0afd21c1c234
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-09-24 08:39:31 +0200

    Curs Spring - Secció 7 (Interceptores HTTP Horario de acceso a clientes) - Classe 98 (Creando proyecto y controller)

---
+++ Spring/springboot-horario/src/main/java/com/roger/curs/springboot/calendar/interceptor/springboot_horario/controllers/AppController.java
@@ -0,0 +1,23 @@
+package com.roger.curs.springboot.calendar.interceptor.springboot_horario.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/app")
+public class AppController {
+
+    @GetMapping("/foo")
+    public ResponseEntity<?> foo() {
+        Map<String, Object> data = new HashMap<>();
+        data.put("title", "Bienvenidos al sistema de atención");
+        data.put("time", new Date());
+        return ResponseEntity.ok(data);
+    }
+}
