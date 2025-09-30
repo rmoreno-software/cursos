cad87d3b86339399a8d30df6e32272c9d03d7f51
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-09-30 08:39:18 +0200

    Curs Spring - Secció 11 (JPA e Hibernate - Asociaciones) - Classe 153 (Creando nuevo proyecto y classes entities)

---
+++ Spring/springboot-jpa-relationship/src/main/java/com/roger/curs/springboot/jpa/springboot_jpa_relationship/entities/Client.java
@@ -0,0 +1,48 @@
+package com.roger.curs.springboot.jpa.springboot_jpa_relationship.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastname;
+
+    public Client() {
+    }
+
+    public Client(Long id, String name, String lastname) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
---
+++ Spring/springboot-jpa-relationship/src/main/java/com/roger/curs/springboot/jpa/springboot_jpa_relationship/entities/Invoice.java
@@ -0,0 +1,48 @@
+package com.roger.curs.springboot.jpa.springboot_jpa_relationship.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="invoices")
+public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private String total;
+
+    public Invoice() {
+    }
+
+    public Invoice(Long id, String description, String total) {
+        this.id = id;
+        this.description = description;
+        this.total = total;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+}
