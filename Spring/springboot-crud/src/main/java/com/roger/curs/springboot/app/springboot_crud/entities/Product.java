9ab658fdc291711a2450fe8b8d5a2307b88c8be6
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-10-02 14:25:42 +0200

    Curs Spring - Secció 12 (Spring Web API RESTful - CRUD) - Classe 181 (Creando clases entity y repository)

---
+++ Spring/springboot-crud/src/main/java/com/roger/curs/springboot/app/springboot_crud/entities/Product.java
@@ -0,0 +1,48 @@
+package com.roger.curs.springboot.app.springboot_crud.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Integer price;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ Spring/springboot-crud/src/main/java/com/roger/curs/springboot/app/springboot_crud/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.roger.curs.springboot.app.springboot_crud.repositories;
+
+import com.roger.curs.springboot.app.springboot_crud.entities.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
