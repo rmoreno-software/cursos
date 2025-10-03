65e1b01af76f296d386a980ae006c245811086a0
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-10-03 09:43:15 +0200

    Curs Spring - Secció 12 (Spring Web API RESTful - CRUD) - Classe 183 (Añadiendo el componente RESTful Controller)

---
+++ Spring/springboot-crud/src/main/java/com/roger/curs/springboot/app/springboot_crud/controllers/ProductController.java
@@ -0,0 +1,61 @@
+package com.roger.curs.springboot.app.springboot_crud.controllers;
+
+import com.roger.curs.springboot.app.springboot_crud.entities.Product;
+import com.roger.curs.springboot.app.springboot_crud.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> list() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> view(@PathVariable Long id) {
+        Product product = productService.findById(id).orElse(null);
+
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    Map.of("status", "error", "message", "product not found")
+            );
+        }
+
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.save(product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Product product = productService.findById(id).orElse(null);
+
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    Map.of("status", "error", "message", "product not found")
+            );
+        }
+
+        return ResponseEntity.ok(productService.delete(product));
+    }
+
+}
