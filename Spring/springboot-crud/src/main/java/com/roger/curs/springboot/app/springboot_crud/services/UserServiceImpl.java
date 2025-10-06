f8f8af57b74dce653c22249119e2634bdb60d4a2
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-10-06 13:23:06 +0200

    Curs Spring - Secció 13 (Backend Users Spring Security JWT) - Classe 197 (Asignando roles y encriptando password con Bcrypt)

--- Spring/springboot-crud/src/main/java/com/roger/curs/springboot/app/springboot_crud/services/UserServiceImpl.java
+++ Spring/springboot-crud/src/main/java/com/roger/curs/springboot/app/springboot_crud/services/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.roger.curs.springboot.app.springboot_crud.services;
 
+import com.roger.curs.springboot.app.springboot_crud.entities.Role;
 import com.roger.curs.springboot.app.springboot_crud.entities.User;
 import com.roger.curs.springboot.app.springboot_crud.repositories.RoleRepository;
 import com.roger.curs.springboot.app.springboot_crud.repositories.UserRepository;
@@ -8,7 +9,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -32,6 +35,18 @@ public class UserServiceImpl implements UserService{
     @Override
     @Transactional
     public User save(User user) {
+        Optional<Role> optRole = roleRepository.findByName("ROLE_USER");
+        List<Role> roles = new ArrayList<>();
+
+        optRole.ifPresent(roles::add);
+
+        if (user.isAdmin()) {
+            Optional<Role> optRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+            optRoleAdmin.ifPresent(roles::add);
+        }
+
+        user.setRoles(roles);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
