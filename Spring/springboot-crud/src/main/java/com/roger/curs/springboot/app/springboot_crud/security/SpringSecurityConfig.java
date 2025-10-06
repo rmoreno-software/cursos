e5fcc5985438196e1998faed8ee9eacb2f6144a8
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-10-06 13:37:55 +0200

    Curs Spring - Secció 13 (Backend Users Spring Security JWT) - Classe 199 (Configurando reglas de seguridad http en los endpoint)

--- Spring/springboot-crud/src/main/java/com/roger/curs/springboot/app/springboot_crud/security/SpringSecurityConfig.java
+++ Spring/springboot-crud/src/main/java/com/roger/curs/springboot/app/springboot_crud/security/SpringSecurityConfig.java
@@ -2,8 +2,11 @@ package com.roger.curs.springboot.app.springboot_crud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -11,6 +14,17 @@ public class SpringSecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests((authz) -> authz
+                .requestMatchers("/users").permitAll()
+                .anyRequest().authenticated())
+                .csrf(config -> config.disable())
+                .sessionManagement(management ->
+                        management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 
 }
