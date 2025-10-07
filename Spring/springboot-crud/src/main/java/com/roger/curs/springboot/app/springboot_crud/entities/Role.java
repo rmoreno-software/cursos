c6a2af5d8fcc8c26f23c3fdca9958d1530d48201
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-10-07 09:00:05 +0200

    Curs Spring - Secció 13 (Backend Users Spring Security JWT) - Classe 202 (ManyToMany Bidireccional revisando error Bucle)

--- Spring/springboot-crud/src/main/java/com/roger/curs/springboot/app/springboot_crud/entities/Role.java
+++ Spring/springboot-crud/src/main/java/com/roger/curs/springboot/app/springboot_crud/entities/Role.java
@@ -1,6 +1,11 @@
 package com.roger.curs.springboot.app.springboot_crud.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "roles")
@@ -13,10 +18,16 @@ public class Role {
     @Column(unique = true)
     private String name;
 
+    @JsonIgnoreProperties({"roles", "handler", "hibernateLazyInitializer"})
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
     public Role() {
+        this.users = new ArrayList<>();
     }
 
     public Role(String name) {
+        this();
         this.name = name;
     }
 
@@ -34,5 +45,25 @@ public class Role {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
--- Spring/springboot-crud/src/main/java/com/roger/curs/springboot/app/springboot_crud/entities/User.java
+++ Spring/springboot-crud/src/main/java/com/roger/curs/springboot/app/springboot_crud/entities/User.java
@@ -1,11 +1,14 @@
 package com.roger.curs.springboot.app.springboot_crud.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
@@ -29,6 +32,7 @@ public class User {
     @Transient
     private boolean admin;
 
+    @JsonIgnoreProperties({"users", "handler", "hibernateLazyInitializer"})
     @ManyToMany
     @JoinTable(
             name = "users_roles",
@@ -38,9 +42,11 @@ public class User {
     private List<Role> roles;
 
     public User() {
+        this.roles = new ArrayList<>();
     }
 
     public User(String username, String password) {
+        this();
         this.username = username;
         this.password = password;
     }
@@ -91,5 +97,17 @@ public class User {
 
     public void setAdmin(boolean admin) {
         this.admin = admin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username);
     }
 }
