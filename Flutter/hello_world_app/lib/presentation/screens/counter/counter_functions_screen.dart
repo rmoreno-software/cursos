845e23f91455a1b5d9fe519ed2d1ee693ede8637
Roger Moreno <roger.moreno.software.engineer@gmail.com>
2025-12-17 09:59:09 +0100

    Flutter - S4 - Widgets personalizados

--- Flutter/hello_world_app/lib/presentation/screens/counter/counter_functions_screen.dart
+++ Flutter/hello_world_app/lib/presentation/screens/counter/counter_functions_screen.dart
@@ -40,37 +40,32 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
         floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            FloatingActionButton(
-              shape: const StadiumBorder(),
-              onPressed: () {
-                  setState(() {
-                    clickCounter++;
-                  });
-              },
-              child: const Icon(Icons.plus_one),
-            ),
+            CustomButton(icon: Icons.plus_one),
             SizedBox(height: 10),
-            FloatingActionButton(
-              shape: const StadiumBorder(),
-              onPressed: () {
-                  setState(() {
-                    clickCounter = 0;
-                  });
-              },
-              child: const Icon(Icons.refresh_outlined),
-            ),
+            CustomButton(icon: Icons.refresh_outlined),
             SizedBox(height: 10),
-            FloatingActionButton(
-              shape: const StadiumBorder(),
-              onPressed: () {
-                  setState(() {
-                    clickCounter--;
-                  });
-              },
-              child: const Icon(Icons.exposure_minus_1_outlined),
-            ),
+            CustomButton(icon: Icons.exposure_minus_1_outlined),
           ],
         )
       );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+
+  final IconData icon;
+
+  const CustomButton({
+    super.key,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      shape: const StadiumBorder(),
+      onPressed: () {},
+      child: Icon(icon),
+    );
   }
 }
