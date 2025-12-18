29e24b18c8907be61858c4e9d759d87f5e60c041
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-12-18 11:36:26 +0100

    Flutter - S5 - Área de los mensajes

--- Flutter/yes_no_app/lib/main.dart
+++ Flutter/yes_no_app/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Yes No App',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 6).theme(),
+      theme: AppTheme(selectedColor: 3).theme(),
       home: ChatScreen(),
     );
   }
--- Flutter/yes_no_app/lib/presentation/chat/chat_screen.dart
+++ Flutter/yes_no_app/lib/presentation/chat/chat_screen.dart
@@ -18,6 +18,31 @@ class ChatScreen extends StatelessWidget {
         title: const Text('Mi amor❤️'),
         centerTitle: false,
       ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return Text('indice: $index');
+                },
+              ),
+            ),
+            Text('Mundo'),
+          ],
+        ),
+      ),
     );
   }
 }
