4d6e054d62c28d3ff902059686578367fb7d2ea0
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-12-18 17:44:00 +0100

    Flutter - S6 - Provider - Gestor de estado

---
+++ Flutter/yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> messages = [
+    Message(text: 'Hola amor!', fromWho: FromWho.me),
+    Message(text: 'Ya Regresaste del trabajo?', fromWho: FromWho.me),
+  ];
+
+  Future<void> sendMessage(String text) async {
+    // TODO: Implementar método
+  }
+}
