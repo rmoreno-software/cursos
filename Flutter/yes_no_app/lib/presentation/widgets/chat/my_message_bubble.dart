b507c3b83862af0e3a1ef3161df4e6e7822741a0
Roger Moreno González <roger.moreno.software.engineer@gmail.com>
2025-12-18 11:50:00 +0100

    Flutter - S5 - Mis mensajes - Burbuja de chat

---
+++ Flutter/yes_no_app/lib/presentation/widgets/chat/my_message_bubble.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MyMessageBubble extends StatelessWidget {
+  const MyMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: colors.primary,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Qui fugiat', style: TextStyle(color: Colors.white)),
+          ),
+        ),
+        SizedBox(height: 10),
+      ],
+    );
+  }
+}
